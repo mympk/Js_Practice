736b5ad55ede7955253695f4da0dec579e12221f
Mushfik Yeasir Pramanik <45345348+mympk@users.noreply.github.com>
2022-12-02 18:00:16 +0600

    Basic like let string etc

---
+++ Basic.js
@@ -0,0 +1,69 @@
+"use strict";
+
+//Printing something
+console.log('Today is 2nd December & its my birthday');
+var myAge= 25;
+myAge=24;
+console.log("My age is =" +myAge);
+
+ //// Constant Value
+let pii=3.1416
+console.log(pii);
+pii=3.1416+3
+console.log(pii)
+
+const pi=3.1416
+console.log([pi]) //[3.1416]
+console.log({pi}) // {pi: 3.1416}
+console.log(pi**2)
+
+
+/////String
+let  today= "Friday"
+console.log(today.length)
+console.log(today[2])
+console.log(today[today.length-1])
+let todayUpdate="s"
+console.log(today+todayUpdate)
+
+
+////trim() Remove space from variable's forward's ans backward's
+let name=" Mission Yeasir Pramanik   "
+name=name.trim()
+console.log(name)
+
+
+/////toUpperCase() 
+let m="Pramanaik"
+m=m.toUpperCase()
+console.log(m)
+
+////toLowerCase() 
+let n="PRAMANIK"
+n=n.toLowerCase()
+console.log(n)
+
+/////slice() Rule(Start Index & End Index)
+let p="Yeasir"
+console.log(p.slice(1)) //easir
+console.log(p.slice(1,3)) //ea
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
